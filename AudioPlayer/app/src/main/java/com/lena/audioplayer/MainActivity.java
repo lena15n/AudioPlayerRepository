85560a5bc09b1af575623e9660399e84d222daa9
Elen Naz <ln15ln@rambler.ru>
2016-06-29 23:17:02 +0400

    some changes for MediaBrowserServiceCompat usage, does not work

--- AudioPlayer/app/src/main/java/com/lena/audioplayer/MainActivity.java
+++ AudioPlayer/app/src/main/java/com/lena/audioplayer/MainActivity.java
@@ -1,22 +1,35 @@
 package com.lena.audioplayer;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String APP_PREFERENCES = "mysettings";//файл настроек
+    public static final String STATUS_STATE = "status";//храним
+
+    SharedPreferences sharedPreferences;
+    Status status;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //set status Idle
 
-        //status Idle, Playing, Paused.
+        status = Status.IDLE;
+
+
     }
 
     @Override
     protected void onPause() {
         super.onPause();
+
+        sharedPreferences = this.getSharedPreferences("status", MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString(STATUS_STATE, status.getString());
+        editor.apply();
 
 
     }
@@ -24,5 +37,39 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onResume() {
         super.onResume();
+
+        if (status == null) {
+            sharedPreferences = getSharedPreferences(APP_PREFERENCES, MODE_PRIVATE);
+            String savedStatusValue = sharedPreferences.getString(STATUS_STATE, "Non Status");
+
+
+            switch (savedStatusValue) {
+                case "idle": {
+                    status = Status.IDLE;
+                }
+                break;
+                case "playing": {
+                    status = Status.PLAYING;
+                }
+                break;
+                case "paused": {
+                    status = Status.PAUSED;
+                }
+                break;
+                default: {
+                    status = null;
+                }
+            }
+        }
+
+        TextView textView = (TextView) findViewById(R.id.statusTextView);
+
+        if (textView != null && status != null) {
+            textView.setText(status.getString());
+        }
+
+
+
+
     }
 }
---
+++ AudioPlayer/app/src/main/java/com/lena/audioplayer/Status.java
@@ -0,0 +1,18 @@
+package com.lena.audioplayer;
+
+/**
+ * Created by Lena on 23.06.2016.
+ */
+public enum Status {
+    IDLE("idle"),
+    PLAYING("playing"),
+    PAUSED("paused");
+
+    private final String state;
+
+    Status(String state) {
+        this.state = state;
+    }
+
+    public String getString() { return state; }
+}
