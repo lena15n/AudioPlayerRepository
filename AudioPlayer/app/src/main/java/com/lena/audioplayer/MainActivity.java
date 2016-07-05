3357ab2840f1d177b59d47faf09afc8c6ae02c62
Elen Naz <ln15ln@rambler.ru>
2016-07-05 20:57:21 +0400

    Add correct status messages and button texts

--- AudioPlayer/app/src/main/java/com/lena/audioplayer/MainActivity.java
+++ AudioPlayer/app/src/main/java/com/lena/audioplayer/MainActivity.java
@@ -1,13 +1,18 @@
 package com.lena.audioplayer;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.IBinder;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     public static final String APP_PREFERENCES = "mysettings";//файл настроек
     public static final String STATUS_STATE = "status";//храним
+    IBinder binder;
 
     SharedPreferences sharedPreferences;
     Status status;
@@ -17,9 +22,39 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        status = Status.IDLE;
+        final TextView statusLabel = (TextView) findViewById(R.id.statusTextView);
+        final Button playButton = (Button) findViewById(R.id.playButton);
 
+        if (playButton != null && statusLabel != null) {
+            playButton.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
+                    switch (status) {
+                        case IDLE: {
+                            status = Status.PLAYING;
+                            statusLabel.setText(R.string.status_playing);
+                            playButton.setText(R.string.button_pause);
+                        }
+                        break;
+                        case PLAYING: {
+                            status = Status.PAUSED;
+                            statusLabel.setText(R.string.status_paused);
+                            playButton.setText(R.string.button_play);
+                        }
+                        break;
+                        case PAUSED: {
+                            status = Status.PLAYING;
+                            statusLabel.setText(R.string.status_playing);
+                            playButton.setText(R.string.button_pause);
+                        }
+                        break;
+                    }
+                }
+            });
 
+            status = Status.IDLE;
+            statusLabel.setText(R.string.status_idle);
+            playButton.setText(R.string.button_play);
+        }
     }
 
     @Override
@@ -38,14 +73,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onResume() {
         super.onResume();
 
-        if (status == null) {
+        /*if (status == null) {
             sharedPreferences = getSharedPreferences(APP_PREFERENCES, MODE_PRIVATE);
             String savedStatusValue = sharedPreferences.getString(STATUS_STATE, "Non Status");
 
 
             switch (savedStatusValue) {
                 case "idle": {
-                    status = Status.IDLE;
+                    status = IDLE;
                 }
                 break;
                 case "playing": {
@@ -60,15 +95,19 @@ public class MainActivity extends AppCompatActivity {
                     status = null;
                 }
             }
-        }
+        }*/
 
-        TextView textView = (TextView) findViewById(R.id.statusTextView);
+        /*TextView textView = (TextView) findViewById(R.id.statusTextView);
 
         if (textView != null && status != null) {
             textView.setText(status.getString());
         }
+*/
+
+        startService(new Intent(this, AudioPlayerService.class));
 
 
+        //binder =  bindService(intent, );
 
 
     }
